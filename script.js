8c95e70a936b732305cfda3e763871a497963eec
emailstudies <mir.s.jani.courses@gmail.com>
2025-08-08 20:52:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let currentTimerId = null;
  */
 function showOnlyFrame(index) {
   const script = `
-    (function () {
+    (function () { 
       var doc = app.activeDocument;
       var animGroup = null;
       var bgLayer = null;
